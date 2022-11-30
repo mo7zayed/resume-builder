4333645483aac36ecb5deadaf0b1b4c7db376ef3
mo7zayed <mohamedzayed907@yahoo.com>
2022-11-30 08:03:44 +0200

    FC

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => 'These credentials do not match our records.',
+            ]);
+        }
+
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'message' => 'user logged successfully',
+            'access_token' => $token->plainTextToken,
+            'user' => new UserResource($user),
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data)->fresh();
+
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'message' => 'user logged successfully',
+            'access_token' => $token->plainTextToken,
+            'user' => new UserResource($user),
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function me()
+    {
+        return response()->json([
+            'message' => 'user info retrieved successfully',
+            'user' => new UserResource(auth()->user()),
+        ]);
+    }
+
+    public function verify()
+    {
+        return response()->json([
+            'message' => 'token is valid',
+            'valid' => true,
+        ]);
+    }
+
+    public function refresh()
+    {
+        /**
+         * @var User
+         */
+        $user = auth()->user();
+
+        $tokenName = $user->currentAccessToken()->name;
+
+        $user->currentAccessToken()->delete();
+
+        $token = $user->createToken($tokenName);
+
+        return response()->json([
+            'message' => 'tokens refreshed successfully',
+            'access_token' => $token->plainTextToken,
+            'user' => new UserResource($user),
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'logged out successfully',
+        ]);
+    }
+}
